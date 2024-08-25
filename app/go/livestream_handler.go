19a471c4698f024e52f36aec1cb97765498713b6
Sota Matsuo <59910028+EringiShimeji@users.noreply.github.com>
2024-08-25 17:39:35 +0900

    Revert "debug"
    This reverts commit fdda67a3a215bcdcdc19ec6e5ac5cd027a48ccf4.

--- app/go/livestream_handler.go
+++ app/go/livestream_handler.go
@@ -481,12 +481,12 @@ func fillLivestreamResponse(ctx context.Context, tx *sqlx.Tx, livestreamModel Li
 	ownerModel, _ := cache.getUserModel(livestreamModel.UserID)
 	owner, err := fillUserResponse(ctx, tx, ownerModel)
 	if err != nil {
-		return Livestream{}, fmt.Errorf("error occured in fillUserResponse: %s", err.Error())
+		return Livestream{}, err
 	}
 
 	tags, err := getLivestreamTags(ctx, tx, livestreamModel.ID)
 	if err != nil {
-		return Livestream{}, fmt.Errorf("error occured in getLivestreamTags: %s", err.Error())
+		return Livestream{}, err
 	}
 
 	livestream := Livestream{
